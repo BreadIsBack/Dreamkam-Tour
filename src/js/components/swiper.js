909b00e61cffeefb629bf69603d957c38d5d5e5c
BreadIsBack <igleb7117@gmail.com>
2024-08-30 00:01:48 +0300

    added first day

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -12,31 +12,38 @@ const swiperHeroBg = new Swiper('.hero__swiper-background', {
   autoplay: {
     delay: 5000,
   },
+  loop: true,
 });
 
+const controlSearch = () => {
+document.querySelectorAll('.swiper-main__prev');
+
+}
+
 const swiperHero = new Swiper('.swiper-main', {
+
   slidesPerView: 'auto',
   speed: 500,
   loop: true,
-  spaceBetween: 35,
   keyboard: {
     enabled: true,
     onlyInViewport: true,
     pageUpDown: true,
   },
+
   navigation: {
     prevEl: '.swiper-main__prev',
     nextEl: '.swiper-main__next',
     clickable: true,
   },
-   breakpoints: {
-    320: {
-      spaceBetween: 17
-    },
-    576: {
-      spaceBetween: 35
-    }
-  },
+  //  breakpoints: {
+  //   320: {
+  //     spaceBetween: 17
+  //   },
+  //   576: {
+  //     spaceBetween: 35
+  //   },
+  // },
 
   on: {
     slideChange: function () {
@@ -44,3 +51,4 @@ const swiperHero = new Swiper('.swiper-main', {
     }
   },
 });
+
