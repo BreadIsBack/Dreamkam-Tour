e82f20eb95a26ba7e98ed0f404c18495e0f97b7d
BreadIsBack <igleb7117@gmail.com>
2024-09-03 00:54:46 +0300

    days sections width changes, sliders changes

--- src/js/components/relocated-text.js
+++ src/js/components/relocated-text.js
@@ -3,15 +3,19 @@ const nextPlace = document.querySelector('.second-day__content');
 const originPlace = document.querySelector('.second-day__slider-container');
 
 window.addEventListener('resize', () => {
-  if (window.innerWidth < 861) {
+  console.log(window.innerWidth);
+})
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth <= 860) {
     nextPlace.appendChild(relocatedText);
-  } else if (window.innerWidth > 860) {
+  } else if (window.innerWidth >= 861) {
     originPlace.appendChild(relocatedText);
   }
 });
 
 const isDesktop = () => {
-  if (window.innerWidth < 861) {
+  if (window.innerWidth <= 860) {
     nextPlace.appendChild(relocatedText);
   }
 
