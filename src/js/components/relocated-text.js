d0c711a2b6cae772625b3255c1535c26c349ea88
BreadIsBack <igleb7117@gmail.com>
2024-11-04 23:31:20 +0300

    added 7 and 8 days, online payment and some changes

--- src/js/components/relocated-text.js
+++ src/js/components/relocated-text.js
@@ -3,10 +3,6 @@ const nextPlace = document.querySelector('.second-day__content');
 const originPlace = document.querySelector('.second-day__slider-container');
 
 window.addEventListener('resize', () => {
-  console.log(window.innerWidth);
-})
-
-window.addEventListener('resize', () => {
   if (window.innerWidth <= 860) {
     nextPlace.appendChild(relocatedText);
   } else if (window.innerWidth >= 861) {
